e3bf6ca4bdda3cdc79b0140751d6270d85fb160a
Tom Hutton <thomaswkhutton@gmail.com>
2018-08-18 11:46:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+var app = new express();
+var port = 3000 || process.ENV.PORT;
+
+app.use(express.static(__dirname + "/public"));
+app.set("views", "views")
+app.set("partials", "views/partials")
+app.set('view engine', 'ejs');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('home', {title: "Home Page"});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: "About - Up & to the Right"});
+});
+
+app.post('/hours', (req, res) => {
+  username = req.body.username;
+  res.render('hours', {title: "Enter Hours", username: username});
+});
+
+app.post('/submit', (req, res) => {
+  res.render('submit', {title: "Hours successfully submitted!"})
+});
+
+app.listen(port, () => {
+  console.log('The server has started!');
+});
